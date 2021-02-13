02505a1f596be10992841a8974ef17b3fe5579f0
Simen Eilevstjønn <simene@lyse.net>
2021-02-13 10:48:54 +0100

    Create unit test project

---
+++ SendsmsWebTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SendsmsWebTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
