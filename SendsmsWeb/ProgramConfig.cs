5173f633096c9db05ff3719d92f06ed047a4cddb
Simen Eilevstjønn <simene@lyse.net>
2021-02-13 15:51:10 +0100

    Add Twilio keys to config

--- SendsmsWeb/ProgramConfig.cs
+++ SendsmsWeb/ProgramConfig.cs
@@ -49,9 +49,49 @@ namespace SendsmsWeb
 
             JObject ParsedConfig = JObject.Parse(ConfigFile);
 
+            try
+            {
+                // Get the file for the current OS
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    IsWindows = false;
+                    ConfigFile = File.ReadAllText("/etc/sendsms/config.json");
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    IsWindows = true;
+                    ConfigFile = File.ReadAllText("C:\\Program Files\\sendsms\\config.json");
+                }
+                else
+                {
+                    throw new Exception("Unsupported operating system");
+                }
+            }
+            catch (Exception e)
+            {
+                if (e.Message == "Unsupported operating system") throw;
+
+                throw new Exception("Could not find config file at " + IsWindows switch
+                {
+                    false => "/etc/sendsms/config.json",
+                    true => "C:\\Program Files\\sendsms\\config.json"
+                });
+            }
+
+            JObject TwilioConfig = JObject.Parse(ConfigFile);
+
             // Set the properties
             ConnectionString = (string)ParsedConfig["ConnectionString"] ?? throw new Exception("ConnectionString is missing or invalid");
-            SendsmsLocation = (string)ParsedConfig["SendsmsLocation"] ?? throw new Exception("SendsmsLocation is missing or invalid");
+            AuthToken = (string)TwilioConfig["AuthToken"] ?? throw new Exception("AuthToken is missing or invalid");
+            AccountSID = (string)TwilioConfig["AccountSID"] ?? throw new Exception("AccountSID is missing or invalid");
+            ServiceSID = (string)TwilioConfig["ServiceSID"] ?? throw new Exception("ServiceSID is missing or invalid");
+            EnableScheduling = (bool)ParsedConfig["EnableScheduling"];
+
+            if (EnableScheduling)
+            {
+                SendsmsLocation = (string)ParsedConfig["SendsmsLocation"] ?? throw new Exception("SendsmsLocation is missing or invalid");
+            }
+
             EnableContacts = (bool)ParsedConfig["EnableContacts"];
             EnableSenderDictionary = (bool)ParsedConfig["EnableSenderDictionary"];
             Enable1881 = (bool)ParsedConfig["Enable1881"];
@@ -65,10 +105,14 @@ namespace SendsmsWeb
         // Properties
         public string ConnectionString { get; private set; }
         public string SendsmsLocation { get; private set; }
+        public bool EnableScheduling { get; private set; }
         public bool EnableContacts { get; private set; }
         public bool EnableSenderDictionary { get; private set; }
         public bool Enable1881 { get; private set; }
         public string ApiKey1881 { get; private set; }
+        public string AuthToken { get; private set; }
+        public string AccountSID { get; private set; }
+        public string ServiceSID { get; private set; }
 
     }
 }
