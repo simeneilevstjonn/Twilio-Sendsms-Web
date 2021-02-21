7cbd4a156fd81c2e53ef88b84875b102cff6ac8d
Simen Eilevstjønn <simene@lyse.net>
2021-02-21 16:15:05 +0100

    WIP rest controller for contacts

---
+++ SendsmsWeb/Controllers/ContactManagementController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendsmsWeb.Controllers
+{
+    [Route("api/contacts")]
+    [ApiController]
+    public class ContactManagementController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> DeleteContact()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContact()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
