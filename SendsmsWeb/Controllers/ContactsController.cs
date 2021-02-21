4c5c0f2aacbfffb6803cdee938d76d2d4fe6179b
Simen Eilevstjønn <simene@lyse.net>
2021-02-21 16:14:45 +0100

    MVC for contacts

---
+++ SendsmsWeb/Controllers/ContactsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendsmsWeb.Contacts;
+using SendsmsWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendsmsWeb.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public async Task<IActionResult> Index() => View(new ContactsViewModel
+        {
+            Contacts = string.IsNullOrWhiteSpace(Request.Query["tag"]) switch
+            {
+                true => await ContactsDbInterface.GetAllContacts(),
+                false => await ContactsDbInterface.GetContactsFromTags(new List<string> { Request.Query["tag"] }, true)
+            },
+            Tags = await ContactsDbInterface.GetAllTags()
+        });
+
+        public IActionResult BuildContact()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SendsmsWeb/Models/ContactsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SendsmsWeb.Contacts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendsmsWeb.Models
+{
+    public class ContactsViewModel
+    {
+        public List<Contact> Contacts { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
